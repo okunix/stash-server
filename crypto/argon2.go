c511e528ce7ff1f13372eeecf40cf4c89687e86d
yusuf <yosa12978@gmail.com>
2026-02-20 23:39:58 +0300

    argon2 update

--- crypto/argon2.go
+++ crypto/argon2.go
@@ -2,13 +2,22 @@ package crypto
 
 import (
 	"crypto/subtle"
-	"encoding/hex"
+	"encoding/base64"
+	"fmt"
+	"regexp"
+	"strconv"
 
 	"golang.org/x/crypto/argon2"
 )
 
+var (
+	argon2EncodingRegex = regexp.MustCompile(
+		`^\$argon2id\$v=(?P<v>\d+)\$m=(?P<d>\d+),t=(?P<t>\d+),p=(?P<p>\d+)\$(?P<salt>\S+?)\$(?P<hash>\S+)$`,
+	)
+)
+
 type KDF interface {
-	DeriveKey(password, salt []byte) (Key, error)
+	DeriveKey(password []byte) (Key, error)
 	Compare(a, b []byte) bool
 }
 
@@ -23,15 +32,30 @@ type argon2IDKey struct {
 }
 
 func (a argon2IDKey) String() string {
-	return hex.EncodeToString(a.key)
+	saltb64 := base64.RawStdEncoding.EncodeToString(a.salt)
+	keyb64 := base64.RawStdEncoding.EncodeToString(a.key)
+	encoded := fmt.Sprintf(
+		"$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s",
+		argon2.Version,
+		a.memory,
+		a.time,
+		a.threads,
+		saltb64,
+		keyb64,
+	)
+	return encoded
 }
-func (a argon2IDKey) Bytes() []byte { return a.key }
+
+func (a argon2IDKey) Bytes() []byte {
+	return a.key
+}
 
 type Argon2ID struct {
 	time    uint32
 	memory  uint32
 	threads uint8
 	keyLen  uint32
+	salt    []byte
 }
 
 type Argon2IDOption func(ar *Argon2ID) error
@@ -64,6 +88,13 @@ func WithTime(time uint32) Argon2IDOption {
 	}
 }
 
+func WithSalt(salt []byte) Argon2IDOption {
+	return func(ar *Argon2ID) error {
+		ar.salt = salt
+		return nil
+	}
+}
+
 func newDefaultArgon2ID() Argon2ID {
 	return Argon2ID{
 		time:    1,
@@ -73,8 +104,9 @@ func newDefaultArgon2ID() Argon2ID {
 	}
 }
 
-func NewArgon2ID(opts ...Argon2IDOption) (KDF, error) {
+func NewArgon2ID(salt []byte, opts ...Argon2IDOption) (KDF, error) {
 	argon2 := newDefaultArgon2ID()
+	argon2.salt = salt
 	for _, opt := range opts {
 		if err := opt(&argon2); err != nil {
 			return nil, err
@@ -83,10 +115,26 @@ func NewArgon2ID(opts ...Argon2IDOption) (KDF, error) {
 	return &argon2, nil
 }
 
-func (s *Argon2ID) DeriveKey(password, salt []byte) (Key, error) {
+func NewArgon2IDFromString(s string) (kdf KDF, hash []byte, err error) {
+	matches := argon2EncodingRegex.FindStringSubmatch(s)
+	salt, _ := base64.RawStdEncoding.DecodeString(matches[5])
+	hash, _ = base64.RawStdEncoding.DecodeString(matches[6])
+	memory, _ := strconv.ParseUint(matches[2], 10, 32)
+	time, _ := strconv.ParseUint(matches[3], 10, 32)
+	threads, _ := strconv.ParseUint(matches[4], 10, 8)
+	kdf, err = NewArgon2ID(
+		salt,
+		WithMemory(uint32(memory)),
+		WithThreads(uint8(threads)),
+		WithTime(uint32(time)),
+	)
+	return
+}
+
+func (s *Argon2ID) DeriveKey(password []byte) (Key, error) {
 	return argon2IDKey{
 		Argon2ID: *s,
-		key:      argon2.IDKey(password, salt, s.time, s.memory, s.threads, s.keyLen),
+		key:      argon2.IDKey(password, s.salt, s.time, s.memory, s.threads, s.keyLen),
 	}, nil
 }
 
---
+++ crypto/argon2_test.go
@@ -0,0 +1,36 @@
+package crypto
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestArgon2(t *testing.T) {
+	password := []byte("helloworld")
+	salt := []byte("aaaaaaaa")
+	kdf, err := NewArgon2ID(
+		salt,
+		WithMemory(64*1024),
+		WithKeyLen(32),
+		WithTime(1),
+		WithThreads(4),
+	)
+	assert.Nil(t, err)
+	key1, err := kdf.DeriveKey(password)
+	assert.Nil(t, err)
+
+	key2, err := kdf.DeriveKey(password)
+	assert.Nil(t, err)
+	assert.Equal(t, key1.String(), key2.String())
+
+	t.Log(key1)
+	t.Log(key2)
+
+	kdf, hash, err := NewArgon2IDFromString(key1.String())
+	key3, err := kdf.DeriveKey(password)
+	assert.Nil(t, err)
+
+	assert.Equal(t, key2, key3)
+	assert.Equal(t, string(hash), string(key2.Bytes()))
+}
