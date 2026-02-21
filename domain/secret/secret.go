f3e21e150513aa4102fe2b666db6fe0cc12541ec
yusuf <yosa12978@gmail.com>
2026-02-21 13:31:32 +0300

    completed secret structure

--- domain/secret/secret.go
+++ domain/secret/secret.go
@@ -2,14 +2,18 @@ package secret
 
 import (
 	"context"
+	"encoding/json"
+	"sync"
 	"time"
 
 	"github.com/google/uuid"
+	"gitlab.com/stash-password-manager/stash-server/crypto"
 )
 
 // in-memory data structure
 type Secret struct {
-	MasterKey  string            `json:"master_key"`
+	mu         sync.RWMutex
+	MasterKey  []byte            `json:"master_key"`
 	Data       map[string]string `json:"data"`
 	UnlockedAt time.Time         `json:"unlocked_at"`
 }
@@ -17,7 +21,7 @@ type Secret struct {
 type AddSecretParams struct {
 	StashID      uuid.UUID
 	MaintainerID uuid.UUID
-	MasterKey    string
+	MasterKey    []byte
 	Data         map[string]string
 }
 
@@ -33,4 +37,43 @@ type Repository interface {
 	AddSecret(ctx context.Context, params AddSecretParams) error
 	RemoveSecret(ctx context.Context, params RemoveSecretParams) error
 	GetSecret(ctx context.Context, params GetSecretParams) (*Secret, error)
+}
+
+func (s *Secret) Seal() ([]byte, error) {
+	cipher := crypto.AESGCM()
+	data, _ := json.Marshal(s.Data)
+	return cipher.Encrypt(s.MasterKey, data)
+}
+
+func NewFromCipher(key, ciphertext []byte) (Secret, error) {
+	cipher := crypto.AESGCM()
+	plaintext, err := cipher.Decrypt(key, ciphertext)
+	if err != nil {
+		return Secret{}, err
+	}
+	var data map[string]string
+	err = json.Unmarshal([]byte(plaintext), &data)
+	return Secret{
+		MasterKey:  key,
+		Data:       data,
+		UnlockedAt: time.Now(),
+	}, err
+}
+
+func (s *Secret) AddEntry(key, value string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.Data[key] = value
+}
+
+func (s *Secret) RemoveEntry(key string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.Data, key)
+}
+
+func (s *Secret) GetEntry(key string) string {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.Data[key]
 }
