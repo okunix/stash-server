b7a32d1e55aa57aac46355f9e799a8f7d44c3d46
yosa12978 <yosa12978@gmail.com>
2026-03-05 23:41:46 +0300

    removed struct

--- internal/core/services/services.go
+++ internal/core/services/services.go
@@ -1,10 +1,1 @@
 package services
-
-import "encoding/json"
-
-type ValidationError map[string]string
-
-func (e ValidationError) Error() string {
-	jsonBytes, _ := json.Marshal(e)
-	return string(jsonBytes)
-}
